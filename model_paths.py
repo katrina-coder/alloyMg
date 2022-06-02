90488b8de777142ba6170285f79a810ba7464f08
katrina-coder <60660630+katrina-coder@users.noreply.github.com>
2022-06-02 17:34:33 +1000

    Update model_paths.py

--- model_paths.py
+++ model_paths.py
@@ -2,16 +2,18 @@ import joblib
 import pickle
 import warnings
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", category=UserWarning)
+
 
 
 if 'google.colab' in str(get_ipython()):
-    model_dir = "alloyMg/models"
+    model_dir = "MgAlloyPublic/models"
 else:
     model_dir = "models"
+    
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
 
-models = {"elongation": joblib.load(f"{model_dir}/ductility"),
+    models = {"elongation": joblib.load(f"{model_dir}/ductility"),
           "tensile": joblib.load(f"{model_dir}/UTS"),
           "yield": joblib.load(f"{model_dir}/YS")
           }
