7023f28232286899b857e995c900a816b58bc8da
katrina-coder <60660630+katrina-coder@users.noreply.github.com>
2022-06-02 17:42:38 +1000

    Update model_paths.py

--- model_paths.py
+++ model_paths.py
@@ -10,10 +10,10 @@ if 'google.colab' in str(get_ipython()):
 else:
     model_dir = "models"
     
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", category=UserWarning)
+# with warnings.catch_warnings():
+#     warnings.simplefilter("ignore", category=UserWarning)
 
-    models = {"elongation": joblib.load(f"{model_dir}/ductility"),
+ models = {"elongation": joblib.load(f"{model_dir}/ductility"),
           "tensile": joblib.load(f"{model_dir}/UTS"),
           "yield": joblib.load(f"{model_dir}/YS")
           }
