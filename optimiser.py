c56422417d73db0d9dc1bea68cb7e741fef54eff
katrina-coder <60660630+katrina-coder@users.noreply.github.com>
2022-06-02 19:13:10 +1000

    Update optimiser.py

--- optimiser.py
+++ optimiser.py
@@ -21,7 +21,7 @@ class MgDatapoint:
         my_input = [100 - sum([*self.range_based_inputs.values()][1:-1])] + [*self.range_based_inputs.values()][1:-1] + ht  
                    # [*self.range_based_inputs.values()] 
         
-        print(np.reshape(my_input, (1, -1)))
+        print(my_input)
         return np.reshape(my_input, (1, -1))
 
     def print(self):
