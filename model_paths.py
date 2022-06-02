d78a8b5b1a2315f9ff8c815053b73013bb9486f3
katrina-coder <60660630+katrina-coder@users.noreply.github.com>
2022-06-02 18:04:00 +1000

    Update model_paths.py

--- model_paths.py
+++ model_paths.py
@@ -13,6 +13,8 @@ else:
 # with warnings.catch_warnings():
 #     warnings.simplefilter("ignore", category=UserWarning)
 
+warnings.filterwarnings('ignore')
+
 models = {"elongation": joblib.load(f"{model_dir}/ductility"),
           "tensile": joblib.load(f"{model_dir}/UTS"),
           "yield": joblib.load(f"{model_dir}/YS")
