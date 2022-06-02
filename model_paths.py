ebbe96c02ba3d75f88c56d9e716eda80cdb5f2fa
katrina-coder <60660630+katrina-coder@users.noreply.github.com>
2022-06-02 14:43:13 +1000

    Update model_paths.py

--- model_paths.py
+++ model_paths.py
@@ -2,11 +2,7 @@ import joblib
 import pickle
 import warnings
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", category=UserWarning)
-    UTS_model = joblib.load(f"{model_dir}/UTS")
-    YS_model= joblib.load(f"{model_dir}/YS")
-    duct_model = joblib.load(f"{model_dir}/ductility")
+
         
 
 
@@ -15,8 +11,13 @@ if 'google.colab' in str(get_ipython()):
     model_dir = "MgAlloyPublic/models"
 else:
     model_dir = "models"
-
     
+    
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    UTS_model = joblib.load(f"{model_dir}/UTS")
+    YS_model= joblib.load(f"{model_dir}/YS")
+    duct_model = joblib.load(f"{model_dir}/ductility")   
     
     
 models = {"elongation": duct_model, 
